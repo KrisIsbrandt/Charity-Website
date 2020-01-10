0a9c2edddfa45a9e253aacbd021a259252d40de0
Krzysztof Isbrandt <kris.isbrandt@gmail.com>
2020-01-10 20:38:45 +0100

    Displaying sum of donnations quantities received in view: -Added DonationRepository in field and implemented a method to get only sum of quantities -Implemented jstl tags in index.jsp

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entities.Institution;
+import pl.coderslab.charity.repositories.DonationRepository;
 import pl.coderslab.charity.repositories.InstitutionRepository;
 
 import java.util.List;
@@ -14,16 +15,21 @@ import java.util.List;
 public class HomeController {
 
     private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
 
     @Autowired
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model){
         List<Institution> institutions = institutionRepository.findAll();
         model.addAttribute("institutions", institutions);
+
+        Long numberOfDonations = donationRepository.getSumOfDonationQuantity();
+        model.addAttribute("numberOfDonations", numberOfDonations);
         return "index";
     }
 }
