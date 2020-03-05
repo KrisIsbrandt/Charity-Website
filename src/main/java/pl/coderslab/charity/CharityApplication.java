9b97f256020880411b3187e0558373a0b147678f
Krzysztof Isbrandt <kris.isbrandt@gmail.com>
2020-03-05 20:31:05 +0100

    Emails: -thymeleaf template beans

--- src/main/java/pl/coderslab/charity/CharityApplication.java
+++ src/main/java/pl/coderslab/charity/CharityApplication.java
@@ -3,9 +3,14 @@ package pl.coderslab.charity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
 
 import javax.validation.Validator;
 import java.util.Locale;
@@ -24,8 +29,31 @@ public class CharityApplication {
         localeResolver.setDefaultLocale(new Locale("pl","PL"));
         return localeResolver; }
 
+    @Bean
+    public TemplateEngine emailTemplateEngine() {
+        final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(stringTemplateResolver());
+        templateEngine.setTemplateEngineMessageSource(emailMessageSource());
+        return templateEngine;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource emailMessageSource() {
+        final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("mail/MailMessages");
+        return messageSource;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CharityApplication.class, args);
     }
 
+    //TemplateEngine resolvers
+    private ITemplateResolver stringTemplateResolver() {
+        final StringTemplateResolver templateResolver = new StringTemplateResolver();
+        templateResolver.setOrder(Integer.valueOf(3));
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
 }
